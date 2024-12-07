d2b17fadcf72939d11e6c8f7f61e7e6265e2f387
Ru Al <39805456+rualb@users.noreply.github.com>
2024-12-07 13:12:24 +0400

    -

--- internal/router/router.go
+++ internal/router/router.go
@@ -104,6 +104,8 @@ func initSys(e *echo.Echo, appService service.AppService) {
 
 func initDebugController(e *echo.Echo, _ service.AppService) {
 	e.GET(consts.PathImagePingDebugAPI, func(c echo.Context) error { return c.String(http.StatusOK, "pong") })
+	// publicly-available-no-sensitive-data
+	e.GET("/health", func(c echo.Context) error { return c.JSON(http.StatusOK, struct{}{}) })
 
 }
 func initImageSizeController(e *echo.Echo, appService service.AppService) {
